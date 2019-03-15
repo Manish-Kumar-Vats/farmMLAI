2d96d4bcde87d740f3cbc5202aec348ecb2fd70f
Manish Vats <mvats77947794@gmail.com>
2019-03-16 02:39:36 +0530

    Added login and signup UI pages and just connected with the firebase

---
+++ app/src/main/java/com/example/android/farmmlai/Login.java
@@ -0,0 +1,18 @@
+package com.example.android.farmmlai;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().setTitle("Login Form");
+
+
+    }
+}
---
+++ app/src/main/java/com/example/android/farmmlai/Signup.java
@@ -0,0 +1,14 @@
+package com.example.android.farmmlai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().setTitle("Signup Form");
+    }
+}
